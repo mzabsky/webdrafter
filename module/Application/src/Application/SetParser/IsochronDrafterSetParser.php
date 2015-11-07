d2ae9d212a6de5b7d1cf87b0b460e6eb996b98bc
Matěj Zábský <mzabsky@gmail.com>
2015-11-07 22:40:28 +0100

    More improvements to export format.

--- module/Application/src/Application/SetParser/IsochronDrafterSetParser.php
+++ module/Application/src/Application/SetParser/IsochronDrafterSetParser.php
@@ -93,7 +93,7 @@ class IsochronDrafterSetParser
 					$state = "manaCost";
 					break;
 				case "manaCost":
-					$currentCard->manaCost = $data;
+					$currentCard->manaCost = $this->replaceSymbols($data);
 					$state = "types";
 					break;
 				case "types":
@@ -111,7 +111,7 @@ class IsochronDrafterSetParser
 					$state = "rulesText";
 					break;
 				case "rulesText":
-					$currentCard->rulesText = str_replace("///br///", "\n", $data);
+					$currentCard->rulesText = $this->replaceSymbols(str_replace("///br///", "\n", $data));
 					$state = "flavorText";
 					break;
 				case "flavorText":
@@ -138,7 +138,7 @@ class IsochronDrafterSetParser
 					$state = "manaCost2";
 					break;
 				case "manaCost2":
-					$currentCard->manaCost2 = $data;
+					$currentCard->manaCost2 = $this->replaceSymbols($data);
 					$state = "types2";
 					break;
 				case "types2":
@@ -156,7 +156,7 @@ class IsochronDrafterSetParser
 					$state = "rulesText2";
 					break;
 				case "rulesText2":
-					$currentCard->rulesText2 = str_replace("///br///", "\n", $data);
+					$currentCard->rulesText2 = $this->replaceSymbols(str_replace("///br///", "\n", $data));
 					$state = "flavorText2";
 					break;
 				case "flavorText2":
@@ -194,5 +194,68 @@ class IsochronDrafterSetParser
 		if($state != "shape" && $state != "empty") throw new \Exception("Unexpected end of file, was in state " . $state . ".");
 		
 		return $cards;
+	}
+	
+	private function replaceSymbols($str) {
+ 		$str = str_replace("<img src='magic-mana-small-T.png' alt='T' width='14' height='14'>", '[T]', $str);
+ 		$str = str_replace("<img src='magic-mana-small-Q.png' alt='Q' width='14' height='14'>", '[Q]', $str);
+		$str = str_replace("<img src='magic-mana-small-S.png' alt='S' width='14' height='14'>", '[S]', $str);
+		
+		$str = str_replace("<img src='magic-mana-small-X.png' alt='X' width='14' height='14'>", '[X]', $str);
+		$str = str_replace("<img src='magic-mana-small-Y.png' alt='Y' width='14' height='14'>", '[Y]', $str);
+		$str = str_replace("<img src='magic-mana-small-Z.png' alt='Z' width='14' height='14'>", '[Z]', $str);
+		
+		$str = str_replace("<img src='magic-mana-small-0.png' alt='0' width='14' height='14'>", '[0]', $str);
+		$str = str_replace("<img src='magic-mana-small-1.png' alt='1' width='14' height='14'>", '[1]', $str);
+		$str = str_replace("<img src='magic-mana-small-2.png' alt='2' width='14' height='14'>", '[2]', $str);
+		$str = str_replace("<img src='magic-mana-small-3.png' alt='3' width='14' height='14'>", '[3]', $str);
+		$str = str_replace("<img src='magic-mana-small-4.png' alt='4' width='14' height='14'>", '[4]', $str);
+		$str = str_replace("<img src='magic-mana-small-5.png' alt='5' width='14' height='14'>", '[5]', $str);
+		$str = str_replace("<img src='magic-mana-small-6.png' alt='6' width='14' height='14'>", '[6]', $str);
+		$str = str_replace("<img src='magic-mana-small-7.png' alt='7' width='14' height='14'>", '[7]', $str);
+		$str = str_replace("<img src='magic-mana-small-8.png' alt='8' width='14' height='14'>", '[8]', $str);
+		$str = str_replace("<img src='magic-mana-small-9.png' alt='9' width='14' height='14'>", '[9]', $str);
+		$str = str_replace("<img src='magic-mana-small-10.png' alt='10' width='14' height='14'>", '[10]', $str);
+		$str = str_replace("<img src='magic-mana-small-11.png' alt='11' width='14' height='14'>", '[11]', $str);
+		$str = str_replace("<img src='magic-mana-small-12.png' alt='12' width='14' height='14'>", '[12]', $str);
+		$str = str_replace("<img src='magic-mana-small-13.png' alt='13' width='14' height='14'>", '[13]', $str);
+		$str = str_replace("<img src='magic-mana-small-14.png' alt='14' width='14' height='14'>", '[14]', $str);
+		$str = str_replace("<img src='magic-mana-small-15.png' alt='15' width='14' height='14'>", '[15]', $str);
+		$str = str_replace("<img src='magic-mana-small-16.png' alt='16' width='14' height='14'>", '[16]', $str);
+		$str = str_replace("<img src='magic-mana-small-17.png' alt='17' width='14' height='14'>", '[17]', $str);
+		$str = str_replace("<img src='magic-mana-small-18.png' alt='18' width='14' height='14'>", '[18]', $str);
+		$str = str_replace("<img src='magic-mana-small-19.png' alt='19' width='14' height='14'>", '[19]', $str);
+		$str = str_replace("<img src='magic-mana-small-20.png' alt='20' width='14' height='14'>", '[20]', $str);
+		
+		$str = str_replace("<img src='magic-mana-small-W.png' alt='W' width='14' height='14'>", '[W]', $str);
+		$str = str_replace("<img src='magic-mana-small-U.png' alt='U' width='14' height='14'>", '[U]', $str);
+		$str = str_replace("<img src='magic-mana-small-B.png' alt='B' width='14' height='14'>", '[B]', $str);
+		$str = str_replace("<img src='magic-mana-small-R.png' alt='R' width='14' height='14'>", '[R]', $str);
+		$str = str_replace("<img src='magic-mana-small-G.png' alt='G' width='14' height='14'>", '[G]', $str);
+		
+		$str = str_replace("<img src='magic-mana-small-2W.png' alt='2/W' width='16' height='16'>", '[2W]', $str);
+		$str = str_replace("<img src='magic-mana-small-2U.png' alt='2/U' width='16' height='16'>", '[2U]', $str);
+		$str = str_replace("<img src='magic-mana-small-2B.png' alt='2/B' width='16' height='16'>", '[2B]', $str);
+		$str = str_replace("<img src='magic-mana-small-2R.png' alt='2/R' width='16' height='16'>", '[2R]', $str);
+		$str = str_replace("<img src='magic-mana-small-2G.png' alt='2/G' width='16' height='16'>", '[2G]', $str);
+		
+		$str = str_replace("<img src='magic-mana-small-HW.png' alt='H/W' width='16' height='16'>", '[PW]', $str);
+		$str = str_replace("<img src='magic-mana-small-HW.png' alt='H/W' width='16' height='16'>", '[PU]', $str);
+		$str = str_replace("<img src='magic-mana-small-HW.png' alt='H/W' width='16' height='16'>", '[PB]', $str);
+		$str = str_replace("<img src='magic-mana-small-HW.png' alt='H/W' width='16' height='16'>", '[PR]', $str);
+		$str = str_replace("<img src='magic-mana-small-HW.png' alt='H/W' width='16' height='16'>", '[PG]', $str);
+		
+		$str = str_replace("<img src='magic-mana-small-WU.png' alt='W/U' width='16' height='16'>", '[WU]', $str);
+		$str = str_replace("<img src='magic-mana-small-UB.png' alt='U/B' width='16' height='16'>", '[UB]', $str);
+		$str = str_replace("<img src='magic-mana-small-BR.png' alt='B/R' width='16' height='16'>", '[BR]', $str);
+		$str = str_replace("<img src='magic-mana-small-RG.png' alt='R/G' width='16' height='16'>", '[RG]', $str);
+		$str = str_replace("<img src='magic-mana-small-GW.png' alt='G/W' width='16' height='16'>", '[GW]', $str);
+		$str = str_replace("<img src='magic-mana-small-WB.png' alt='W/B' width='16' height='16'>", '[WB]', $str);
+		$str = str_replace("<img src='magic-mana-small-UR.png' alt='U/R' width='16' height='16'>", '[UR]', $str);
+		$str = str_replace("<img src='magic-mana-small-WU.png' alt='B/G' width='16' height='16'>", '[BG]', $str);
+		$str = str_replace("<img src='magic-mana-small-RW.png' alt='R/W' width='16' height='16'>", '[RW]', $str);
+		$str = str_replace("<img src='magic-mana-small-UG.png' alt='U/G' width='16' height='16'>", '[UG]', $str);
+		
+		return $str;
 	}
 }
