0ead4859cde45c4e5f4b7f5bc438f2a8878dbb7e
Matěj Zábský <mzabsky@gmail.com>
2017-01-07 18:00:50 +0100

    Fixed form input size.

--- module/Application/src/Application/Model/SetVersion.php
+++ module/Application/src/Application/Model/SetVersion.php
@@ -185,7 +185,7 @@ class SetVersion implements InputFilterAwareInterface
     								'options' => array(
     										'encoding' => 'UTF-8',
     										'min'      => 0,
-    										'max'      => 5000,
+    										'max'      => 50000,
     								),
     						),
     				),
