548215d4c26aedcb3346ab3b436d9e5f7dbf0e44
Matěj Zábský <mzabsky@gmail.com>
2017-01-30 20:10:51 +0100

    Use mtRand as shuffler for cube packs.

--- module/Application/src/Application/PackGenerator/CubePackGenerator.php
+++ module/Application/src/Application/PackGenerator/CubePackGenerator.php
@@ -5,6 +5,17 @@ use Application\Model\Card;
 
 class CubePackGenerator
 {
+	private function mtShuffle(&$array) {
+		$array = array_values($array);
+		for($i = count($array) - 1; $i > 0; --$i) {
+			$j = mt_rand(0, $i);
+			if($i !== $j) {
+				list($array[$i], $array[$j]) = array($array[$j], $array[$i]);
+			}
+		}
+		return true;
+	}
+	
 	public function GeneratePacks($cards, $numberOfPacks)
 	{
 		$packSize = 15;
@@ -24,7 +35,7 @@ class CubePackGenerator
 		
 		$cards = $cardsArray;
 		
-		shuffle($cards);
+		$this->mtShuffle($cards);
 		
 		$list = array();
 		for($i = 0; $i < $numberOfPacks; $i++)
