21c1b7580786108aff16a8993fd9e9524c7deb9c
Matěj Zábský <mzabsky@gmail.com>
2020-04-11 09:49:59 +0200

    Increased session lifetime.

--- webdrafter/index.php
+++ webdrafter/index.php
@@ -5,7 +5,9 @@
  */
 chdir(dirname(__DIR__));
 
-session_start();
+session_start([
+  'gc_maxlifetime' => 86400
+]);
 
 // Decline static file requests back to the PHP built-in webserver
 if (php_sapi_name() === 'cli-server' && is_file(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))) {
@@ -17,7 +19,7 @@ require 'init_autoloader.php';
 require 'vendor/Google/autoload.php';
 
 // Setup raven
-require_once '/vendor/Raven/Autoloader.php';
+require_once 'vendor/Raven/Autoloader.php';
 Raven_Autoloader::register();
 
 // Run the application!
